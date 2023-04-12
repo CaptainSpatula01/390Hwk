a77f79fa058445f40953b73afa180ad24748969b
CaptainSpatula01 <brycenorris2002@gmail.com>
2023-04-11 20:43:09 -0500

    New HWK

--- HWK#3/ScaryLinkedList/src/ScaryListTest.java
+++
@@ -1,9 +0,0 @@
-public class ScaryListTest {
-    public static void main(String [] args) {
-        System.out.println("\033[0;31mL\033[0;32mi\033[0;33mg\033[0;34mm\033[0;35ma\033[0;36m b\033[0;37ma\033[0;31ml\033[0;32ml\033[0;33m\033[0;34ms");
-
-        LinkMe2 list = new LinkMe2();
-
-        list.buildScaryList();
-    }
-}
